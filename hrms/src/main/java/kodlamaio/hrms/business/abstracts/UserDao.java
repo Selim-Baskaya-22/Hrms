9d2c36043b07581f6cf4e98c013f151cea7ef704
Selim Başkaya <selimbaskaya34@outlook.com>
2021-05-12 23:44:34 +0300

    HrmsProject

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/UserDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.business.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.hrms.entities.concretes.User;
-
-public interface UserDao extends JpaRepository<User, Integer>{
-
-}
