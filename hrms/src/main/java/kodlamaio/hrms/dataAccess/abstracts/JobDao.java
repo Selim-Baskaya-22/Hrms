fbce5d00f41b40937e81876b7ed8d7fbd6732f9f
Selim Başkaya <selimbaskaya34@outlook.com>
2021-05-12 23:44:19 +0300

    Yeni başlayanlar için yorum satırlarına bol bol yer verildi✓

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobDao.java
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobDao.java
@@ -12,10 +12,17 @@ import kodlamaio.hrms.entities.concretes.Job;
     tam olarak oraya bakıyor.JpaRepository icerisindeki methodlari incelerseniz Job alanındaki generic yapı olan T entitysinin    
     aslında bir @Entity annotationsı olması gerekiyor diye kurallar koyulmus. Eger siz Job taki @Entity silerseniz calisma aninda 
     hata aliriz. 
+
   *Integer neyin nesi?
     Orada ise benim primary keyimim veri tipini belirtmis oluyoruz. Bunu nerden tanıyor diye sorarsanız onuda Job'un içersindeki
     @Id annotationundan taniyor. Kurallar ayni @Entity deki gibi isliyor.
   
+  *Peki neden böyle bir yapi kullaniliyor?
+    Bunun aslinda bircok nedeni var. Ben bildiklerimi ve akilda kalici olanlari anlatayim
+    1)Oncelikle tüm crud operasyonlarinda iclerine biz herhangi bir hibernate kodu yazmadigimizda büyük bir yukten kurtuluyoruz
+    2)Yazilim ortamlari(sonucta bir insan oldugumuzdan) hata yapmaya cok müsaittir. Iste bunlarin buyuk bir kismini ortadan
+     kaldirmis oluyoruz. Eger bu yapiyi kullanmasaydik Job yerine herhangi bir sinif yazabilecek ve hatasini bulmakta zorlanacakti.
+     İste bu yapida @Entity kuralini gecemedigi icin calistirma aninda hata vericek ve programci hatasini kolayca farkedebilecektir 
   */
 public interface JobDao extends JpaRepository<Job, Integer>{
 
