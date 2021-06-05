1127bc04662a273dc2c4c48159b531240cbf7001
Selim Başkaya <selimbaskaya34@outlook.com>
2021-06-05 17:31:25 +0300

    DataAccess işlemleri tamamlandı

---
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/LinkDao.java
@@ -0,0 +1,9 @@
+package kodlamaio.hrms.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kodlamaio.hrms.entities.concretes.Link;
+
+public interface LinkDao extends JpaRepository<Link, Integer>{
+
+}
